5bcaacc702a5346bda0cc4069d1ad1123d45515a
Matey Tsilov <matey.tsilov@gateway.one>
2024-11-28 10:53:29 +0200

    Fix LogIn and Register scaffolds and add additional link to AllQuizzes + some design changes on AllQuizzes view

--- AnimeQSystem.Web/Controllers/HomeController.cs
+++ AnimeQSystem.Web/Controllers/HomeController.cs
@@ -4,10 +4,6 @@ namespace AnimeQSystem.Web.Controllers
 {
     public class HomeController : Controller
     {
-        public HomeController()
-        {
-        }
-
         public IActionResult Index()
         {
             return View();
