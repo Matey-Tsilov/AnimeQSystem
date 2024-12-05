604e4e651cbf17cee5975a04cf248c365e25268b
Matey Tsilov <matey.tsilov@gateway.one>
2024-12-05 19:13:05 +0200

    Add MiscHelper class in common project

---
+++ AnimeQSystem.Common/MiscHelper.cs
@@ -0,0 +1,31 @@
+﻿namespace AnimeQSystem.Common
+{
+    /// <summary>
+    /// A helper class for methods which are common for all areas
+    /// </summary>
+    public static class MiscHelper
+    {
+        public static async byte[] ConvertImageToBytes(IFromFile ImageFile)
+        {
+            byte[] ImageData;
+            string imagesFolder = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, @"..\..\..\wwwroot\images"));
+
+            // If there is not an image, use default one
+            if (ImageFile == null)
+            {
+                ImageData = File.ReadAllBytes(Path.Combine(imagesFolder, "users", "defaultuser.jpg"));
+            }
+            else
+            {
+                // Convert the image to byte[]
+                using (var memoryStream = new MemoryStream())
+                {
+                    await ImageFile.CopyToAsync(memoryStream);
+                    ImageData = memoryStream.ToArray(); // Convert to byte[]
+                }
+            }
+
+            return ImageData;
+        }
+    }
+}
